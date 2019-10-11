890c055bc019b5b1ad8dd6fad65e35b4156ba912
Brando <b.preda91@gmail.com>
2019-10-11 14:48:44 +0200

    add variabile per frontend

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require('fs');
+const ejs = require('ejs');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var freeParking = 100
@@ -7,11 +9,21 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => home(res))
   .get('/start_parking', (req, res) => parkTaken(res))
   .get('/info_parking', (req, res) => info(res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+
+
+
+function home(response){
+	let template = fs.readFileSync('views/pages/index.ejs').toString();	
+	const output = ejs.render(template, {parkings: freeParking});
+
+	response.end(output);
+	res.render('pages/index')
+}
 
 function parkTaken(response){
 	freeParking--;
